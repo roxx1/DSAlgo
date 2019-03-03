f0ea238024ad99568a23ca1d1a7cc2add422b5a8
roxx1 <akshatjindal1993@gmail.com>
2019-03-03 15:47:05 +0530

    two string rotations

--- src/problem_solving/string/TwoStringsRotationOfEachOther.java
+++ src/problem_solving/string/TwoStringsRotationOfEachOther.java
@@ -3,16 +3,21 @@ package problem_solving.string;
 public class TwoStringsRotationOfEachOther {
 
     public static void main(String[] args) {
-        check2StringsRotationOfEachOther("ravi", "arar");
+        boolean b = check2StringsRotationOfEachOther("ravi", "vira");
+        System.out.println(b);
     }
 
-    private static void check2StringsRotationOfEachOther(String s1, String s2) {
+    private static boolean check2StringsRotationOfEachOther(String s1, String s2) {
         if (s1.length() != s2.length()) {
-            return;
+            return false;
         }
 
-        String s3 = s1 + s2;
+        String s3 = s1 + s1;
         // if s2 is a substring of s3 then true else false
+        if(s3.contains(s2)){
+            return true;
+        }
+        return false;
     }
 
 }
