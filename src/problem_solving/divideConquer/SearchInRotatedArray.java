12db9f1c6dbe4b4452c8c7aaacb9ae37e0723603
roxx1 <akshatjindal1993@gmail.com>
2019-04-08 04:07:04 +0530

    added approach 2  code for search in rotated array

--- src/problem_solving/divideConquer/SearchInRotatedArray.java
+++ src/problem_solving/divideConquer/SearchInRotatedArray.java
@@ -6,9 +6,45 @@ public class SearchInRotatedArray {
 
     public static void main(String[] args) {
         int arr[] = new int[]{7, 8, 9, 10, 11, 12, 13, 14, 3, 4, 5, 6}; // sorted rotated array
+        int k = 6;
+        findEltInRotatedArrayApproach1(arr, k);
+        int pos = findEltInRotatedArrayApproach2(arr, k, 0, arr.length - 1);
+        System.out.println(pos);
+    }
+
+    private static int findEltInRotatedArrayApproach2(int[] arr, int k, int start, int end) {
+        if (start > end) {
+            return -1;
+        }
+        int mid = (start + end) / 2;
+        if (arr[mid] == k) {
+            return mid;
+        }
+        if (arr[start] <= arr[mid]) { // first half sorted
+            if (k >= arr[start] && k <= arr[mid]) {
+                return Arrays.binarySearch(arr, start, mid + 1, k); // if elt present between first half
+            } else {
+                return findEltInRotatedArrayApproach2(arr, k, mid, end);
+            }
+        } else { // second half sorted
+            if (k >= arr[mid] && k <= arr[end]) {
+                return Arrays.binarySearch(arr, mid, end + 1, k); // if elt present between second half
+            } else {
+                return findEltInRotatedArrayApproach2(arr, k, start, mid);
+            }
+        }
+
+
+    }
+
+    private static void findEltInRotatedArrayApproach1(int[] arr, int k) {
+        int pos;
         int pivot = findPivot(arr, 0, arr.length - 1);
         System.out.println(pivot);
-        int k = 6, pos;
+        if (pivot == -1) {
+            System.out.println("No Elt Present");
+            return;
+        }
         if (pivot == arr.length - 1) {
             pos = Arrays.binarySearch(arr, k);
         } else {
