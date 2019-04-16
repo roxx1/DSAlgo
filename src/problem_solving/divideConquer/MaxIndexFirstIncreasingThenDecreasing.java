d48cc66d4ff892556ac854f79e61b265ba03c3fa
roxx1 <akshatjindal1993@gmail.com>
2019-04-16 08:46:55 +0530

    added max inc then decr approach 2

--- src/problem_solving/divideConquer/MaxIndexFirstIncreasingThenDecreasing.java
+++ src/problem_solving/divideConquer/MaxIndexFirstIncreasingThenDecreasing.java
@@ -5,7 +5,20 @@ public class MaxIndexFirstIncreasingThenDecreasing {
     public static void main(String[] args) {
         int arr[] = new int[]{1, 2, 10, 11, 12, 13, 14, 20, 7, 8, 9};
         int index = findIndexFirstIncreasingThenDecreasing(arr, 0, arr.length - 1);
+        int index2 = findIndexFirstIncreasingThenDecreasingApproach2(arr, 0, arr.length - 1);
         System.out.println(index);
+    }
+
+    private static int findIndexFirstIncreasingThenDecreasingApproach2(int[] arr, int start, int end) {
+        int mid = (start + end) / 2;
+        if ((mid == 0 || arr[mid] > arr[mid - 1]) && (mid == arr.length - 1 || arr[mid] > arr[mid + 1])) {
+            return mid;
+        }
+        if (mid > 0 && arr[mid] > arr[mid - 1] && arr[mid] < arr[mid + 1]) {
+            return findIndexFirstIncreasingThenDecreasingApproach2(arr, mid + 1, end);
+        } else {
+            return findIndexFirstIncreasingThenDecreasingApproach2(arr, start, mid);
+        }
     }
 
     private static int findIndexFirstIncreasingThenDecreasing(int[] arr, int start, int end) {
