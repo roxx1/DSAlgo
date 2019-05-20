b5660276c18237a41b4a3374c6c78c479bd888e4
roxx1 <akshatjindal1993@gmail.com>
2019-05-20 08:09:28 +0530

    KPalindrome

---
+++ src/problem_solving/dp/PalindromeByDeletingAtmostKCharacters.java
@@ -0,0 +1,12 @@
+package problem_solving.dp;
+
+public class PalindromeByDeletingAtmostKCharacters {
+    public static void main(String[] args) {
+        String s = "abxa";
+        int k = 1;
+        // Reverse String s ------ sr = axba
+        // find Longest Common Subseq  axa
+        // calculate Diff in length of s and Lcs
+        // if diff <= k then Yes else No
+    }
+}
